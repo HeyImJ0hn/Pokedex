9e37f72686e235ac197dcbcdcba48537b1202215
João <jpires.dev@gmail.com>
2023-04-12 10:40:30 +0100

    Initial commit

---
+++ app/src/main/java/dam_a47471/pokedex/ui/gallery/GalleryViewModel.kt
@@ -0,0 +1,13 @@
+package dam_a47471.pokedex.ui.gallery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GalleryViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is gallery Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ app/src/main/java/dam_a47471/pokedex/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package dam_a47471.pokedex.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
